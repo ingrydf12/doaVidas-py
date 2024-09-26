09cc6816720e5ed90c7f36ef178a7fcfa031d781
Ingryd <ingrydcduarte@gmail.com>
2024-09-25 23:15:34 -0300

    main file with scope

---
+++ main.py
@@ -0,0 +1,22 @@
+#TODO: Menu (separar em função)
+
+print("""
+ ________  ________  ___       ___      ___ ________  ___      ___ ___  ________  ________  ________      
+|\   ____\|\   __  \|\  \     |\  \    /  /|\   __  \|\  \    /  /|\  \|\   ___ \|\   __  \|\   ____\     
+\ \  \___|\ \  \|\  \ \  \    \ \  \  /  / | \  \|\  \ \  \  /  / | \  \ \  \_|\ \ \  \|\  \ \  \___|_    
+ \ \_____  \ \   __  \ \  \    \ \  \/  / / \ \   __  \ \  \/  / / \ \  \ \  \ \\ \ \   __  \ \_____  \   
+  \|____|\  \ \  \ \  \ \  \____\ \    / /   \ \  \ \  \ \    / /   \ \  \ \  \_\\ \ \  \ \  \|____|\  \  
+    ____\_\  \ \__\ \__\ \_______\ \__/ /     \ \__\ \__\ \__/ /     \ \__\ \_______\ \__\ \__\____\_\  \ 
+   |\_________\|__|\|__|\|_______|\|__|/       \|__|\|__|\|__|/       \|__|\|_______|\|__|\|__|\_________/
+                                                                        
+    Programa direcionado ao espaço do doador para entender e incentivar a doação de sangue em Fortaleza.
+                                                                                                          
+   MENU INTERATIVO: PARA ONDE VOCÊ QUER IR?
+   [1] Quem pode doar?
+   [2] Desmistificando a doação
+   [3] Locais de doação em Fortaleza                                                                                
+""")
+
+#TODO: Opcoes basicas do programa -> quem pode doar sangue, os benefícios da doação e os procedimentos envolvidos.
+
+# EXTRA: critérios de elegibilidade e Dicas para Doadores
